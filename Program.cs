33269831bdac9083e57285ce84de320738256896
Chris Ormerod <cormerod@domain.com.au>
2019-11-29 10:30:13 +1100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace util_dynamoupdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DynamoDB Item Deleter");
+
+            
+
+        }
+    }
+}
